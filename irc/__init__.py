b6a6ed288df308011595e1cfbcc6cbe4cca001eb
Jerome Leclanche <jerome@leclan.ch>
2011-02-13 01:57:33 +0000

    Move IRCUser to irc.user submodule

--- irc/__init__.py
+++ irc/__init__.py
@@ -189,35 +189,3 @@ class IRCServer(QTcpSocket):
 		super(IRCServer, self).write(data)
 		self.waitForBytesWritten()
 		self.packetWritten.emit(data)
-
-
-class IRCUser(object):
-	def __init__(self, nick, parent):
-		if "!" in nick:
-			nick, self.__host = nick.split("!")
-		self.__nick= nick
-		self.__parent = parent # server
-	
-	def name(self):
-		"""
-		Alias for nick()
-		"""
-		return self.__nick
-	
-	def nick(self):
-		"""
-		Returns the user's nickname.
-		"""
-		return self.__nick
-	
-	def parent(self):
-		"""
-		Returns the IRCServer the user lives on.
-		"""
-		return self.__parent
-	
-	def write(self, message):
-		"""
-		Sends \a message to the user.
-		"""
-		self.parent().send("PRIVMSG %s :%s" % (self.name(), message))
---
+++ irc/user.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+User logic
+"""
+
+
+class IRCUser(object):
+	def __init__(self, nick, parent):
+		if "!" in nick:
+			nick, self.__host = nick.split("!")
+		self.__nick= nick
+		self.__parent = parent # server
+	
+	def name(self):
+		"""
+		Alias for nick()
+		"""
+		return self.__nick
+	
+	def nick(self):
+		"""
+		Returns the user's nickname.
+		"""
+		return self.__nick
+	
+	def parent(self):
+		"""
+		Returns the IRCServer the user lives on.
+		"""
+		return self.__parent
+	
+	def write(self, message):
+		"""
+		Sends \a message to the user.
+		"""
+		self.parent().send("PRIVMSG %s :%s" % (self.name(), message))
