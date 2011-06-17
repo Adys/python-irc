4084ea972f74a063c03060db00a90b5235e22aaf
Jerome Leclanche <jerome@leclan.ch>
2011-06-17 12:01:59 +0100

    Fix path checks at runtime

--- bin/main.py
+++ bin/main.py
@@ -5,7 +5,9 @@ Python IRC test client
 """
 
 from __future__ import print_function
-import sys; sys.path.append("..")
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir))
 from irc import IRCServer
 from PySide.QtCore import QCoreApplication, QTimer
 
