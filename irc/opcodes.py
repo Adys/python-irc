cbdb7020420768c92d66741d0cfc4f9cb67c0609
Jerome Leclanche <jerome@leclan.ch>
2013-02-18 08:29:50 +0000

    Fix a syntax error on python 3

--- irc/opcodes.py
+++ irc/opcodes.py
@@ -4,7 +4,7 @@ List of opcodes (replies)
 http://www.irchelp.org/irchelp/rfc/chapter6.html
 """
 
-RPL_WELCOME       = 001
+RPL_WELCOME       =   1
 RPL_TOPIC         = 332
 RPL_CHANNELMODEIS = 324
 RPL_CREATIONTIME  = 329
